bbbcc753ec46b67860884034dd0616878f2b6ef2
wangxin <xinxwang@thoughtworks.com>
2020-08-20 20:05:59 +0800

    Play destory particles if enemy die

--- script/Enemy.gd
+++ script/Enemy.gd
@@ -1,7 +1,15 @@
 extends Sprite
 
 var speed = 40
+var destory_particles = preload("res://scene/EnemyDestoryParticles.tscn")
 
+signal create_enemy_destory_particles(destroy_paticles, position)
+
+
+func _ready():
+	if Global.world != null:
+		connect("create_enemy_destory_particles", Global.world, "_on_Create_enemy_destory_particle")
+	pass
 
 func _process(delta):
 	global_position.y += speed * delta
@@ -10,5 +18,6 @@ func _process(delta):
 
 func _on_Hitbox_area_entered(area):
 	if area.is_in_group("Bullet"):
+		emit_signal("create_enemy_destory_particles", destory_particles, global_position)
 		queue_free()
 	pass
--- script/MainScene.gd
+++ script/MainScene.gd
@@ -4,6 +4,15 @@ extends Node2D
 var enemy = preload("res://scene/Enemy.tscn")
 
 
+func _ready():
+	Global.world = self
+	pass
+
+
+func _exit_tree():
+	Global.world = null
+	pass
+
 func _on_Player_create_bullet(bullet, pos):
 	var bullet_instance = bullet.instance()
 	add_child(bullet_instance)
@@ -15,4 +24,10 @@ func _on_EnemyTimer_timeout():
 	var enemy_instance = enemy.instance()
 	add_child(enemy_instance)
 	enemy_instance.global_position = Vector2(rand_range(0, get_viewport_rect().size.x), 0)
+	pass
+
+func _on_Create_enemy_destory_particle(destroy_paticles, position):
+	var destory_particles_instance = destroy_paticles.instance()
+	add_child(destory_particles_instance)
+	destory_particles_instance.global_position = position
 	pass
